83e0ea0463d11412cab8364b672353f110884a74
yunglleung1 <yungl.leung1@gmail.com>
2018-08-04 15:15:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+  @@routes = ["name" => "My name is __", "hometown" => "My hometown is __",
+    "favorite-song" => "My favorite song is __"]
+
+  get '/*' do
+    route_response = @@routes.map do |route|
+      route[params['splat'][0]]
+    end
+    "#{route_response[0]}"
+  end
+
 end
