d09e07bdd6f939d8e32b94799f84ba599666964b
Andrew Och <andrewoch95@gmail.com>
2018-08-06 13:02:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/' do
+    "Hello World"
+  end
+
+  get '/name' do
+    "My name is Andrew Och"
+  end
+
+  get '/hometown' do
+    "My hometown is Scarsdale, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hey Mama"
+  end
+
+
 end
